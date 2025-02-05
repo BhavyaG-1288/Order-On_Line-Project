cc1f699789273e92d1cb59942267fb40ad429e48
BhavyaG-1288 <gbhavya2017@gmail.com>
2025-02-05 20:58:14 +0530

    Basic navbar footer and html pages were created

---
+++ scripts/footer.js
@@ -0,0 +1,10 @@
+
+const footer =()=>{
+    let card =` <div>
+        <h5> ORDER ON-LINE online grocery store<span>&copy; All Rights Reserved 2025</span></h5>
+    </div>`;
+
+    document.getElementById("footer").innerHTML =card;
+};
+
+footer();
---
+++ scripts/navbar.js
@@ -0,0 +1,16 @@
+
+const navbar = ()=> {
+
+    let card =`<div id ="nav-container">
+    <a id ="logo" href ="index.html">ORDER ON-LINE</a>
+    <div id ="nav-links">
+        <a href="signup.html">Signup</a>
+        <a href="login.html">Login</a>
+        <a href="todos.html">Todos</a>
+        <a href="expenses.html">Expenses</a>
+    </div>
+    </div>`;
+
+    document.getElementById("nav").innerHTML =card;
+};
+navbar();
